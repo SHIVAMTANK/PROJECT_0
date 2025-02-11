08b188e3bad6836933974ee30d26b11d1ae1a1b2
SHIVAMTANK <shivamtank8059@gmail.com>
2025-02-11 21:10:55 +0530

    updated

--- VISITOR_MANAGMENT/backEND/routes/hostelWarden/hostelWarden.js
+++ VISITOR_MANAGMENT/backEND/routes/hostelWarden/hostelWarden.js
@@ -1,6 +1,6 @@
 const express = require('express');
 
-const router = express.Router('hostelWarden');
+const router = express.Router();
 
 const auth = require('../../middleware/hostelWardenAuth');
 
